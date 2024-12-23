d5a882258242b8da283f75e943ddde0d97b9e7c3
Raiyan Ahmed <ray38250968@gmail.com>
2024-12-23 16:33:53 +0530

    COMPLETE

--- main.js
+++ main.js
@@ -1,71 +1,76 @@
-const numberInput = document.getElementById("numberField");
 let prevNum = 0,
-    ops = "";
+    ops = "ADDITION",
+    numberField = document.getElementById("numberField"), fix = 0;
 
-console.log(`
-        prevNum : ${prevNum},
-        ops : ${ops},
-        `);
-
-function numpadHit(terms) {
-    console.log(`
-        prevNum : ${prevNum},
-        ops : ${ops},
-        terms : ${terms},
-        `);
-
-    if (
-        terms === "ADDITION" ||
-        terms === "SUBTRACTION" ||
-        terms === "MULTIPLICATION" ||
-        terms === "DIVISON"
-    ) {
-        if (ops === "EQUALS") {
-            prevNum = parseFloat(numberInput.value);
-            numberInput.value = '';
-        }
-        if (ops === "") {
-            ops = terms;
-            prevNum = parseFloat(numberInput.value);
-            numberInput.value = ""
-        } else {
-            let result = operate();
-            prevNum = result;
-            numberInput.value = "";
-        }
-
-    } else if (terms === "DELETE") {
-        numberInput.value = ""
-    } else if (terms === "RESET") {
-        ops = "";
-        prevNum = 0;
-        numberInput.value = "";
-    } else if (terms === "EQUALS") {
-        let result = operate();
-        if (result === "ERROR") {
-            alert('Please specify value!')
-        } else {
-            numberInput.value = result;
-            prevNum = result;
-            ops = "EQUALS";
+const numpadHit = (InputNumber) => {
+    let val = numberField.value;
+    if (numberField.value.slice(-2) === '.0') {
+        val = val.substring(0, val.length - 1);
+        val = val.concat(InputNumber);
+    } else if (InputNumber === '.') {
+        if (!numberField.value.includes('.')) {
+            val = val + '.0';
         }
     } else {
-        numberInput.value = numberInput.value.concat(terms);
+        val = numberField.value.concat(InputNumber);
     }
-    console.log(`
-        CURRENT!
-        prevNum : ${prevNum},
-        ops : ${ops},
-        terms : ${terms},
-        `);
+
+    numberField.value = val;
+};
+
+function EQUALS() {
+    let result = operate().toFixed(fix);
+    if (result === "ERROR") {
+        alert("ERROR");
+    } else {
+        numberField.value = result;
+    }
+    ops = '';
 }
+const Operator = (operator) => {
+    if (!ops === '') {
+        let result = operate().toFixed(fix);
+        if (result === "ERROR") {
+            alert("ERROR");
+        } else {
+            prevNum = result;
+        }
+        numberField.value = "";
+    }
+    else {
+        prevNum = parseFloat(numberField.value);
+        ops = operator;
+        numberField.value = "";
+    }
+};
 
 const operate = () => {
-    let op1 = parseFloat(numberInput.value),
+    let op1 = parseFloat(numberField.value),
         op2 = parseFloat(prevNum);
     if (ops === "ADDITION") return op2 + op1;
     if (ops === "SUBTRACTION") return op2 - op1;
     if (ops === "MULTIPLICATION") return op2 * op1;
     if (ops === "DIVISON") return op2 / op1;
-    else return "ERROR"
+    else return "ERROR";
+};
+
+
+function Del() {
+    if (numberField.value.length <= 0) {
+        return;
+    }
+    let inputNumberVal = numberField.value;
+    if (numberField.value.slice(-2).charAt(0) === '.') {
+        if (inputNumberVal.length > 0) {
+            numberField.value = inputNumberVal.slice(0, -2);
+        } else {
+            numberField = ''
+        }
+    } else {
+        if (inputNumberVal.length > 0) {
+            numberField.value = inputNumberVal.slice(0, -1);
+        } else {
+            numberField = ''
+        }
+    }
 }
