a5732301234e9dfc604388aad9c6ff1dfa59a6b4
Raiyan Ahmed <ray38250968@gmail.com>
2024-12-23 13:11:54 +0530

    ...Logical Change

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 const numberInput = document.getElementById("numberField");
-let prevNum,
-    ops = "", isSolved = false;
+let prevNum = 0,
+    ops = "";
+
+console.log(`
+        prevNum : ${prevNum},
+        ops : ${ops},
+        `);
 
 function numpadHit(terms) {
-    if (isSolved === true) {
-        numberInput.value = '';
-        isSolved = false
-    }
     console.log(`
         prevNum : ${prevNum},
         ops : ${ops},
         terms : ${terms},
         `);
+
     if (
         terms === "ADDITION" ||
         terms === "SUBTRACTION" ||
         terms === "MULTIPLICATION" ||
         terms === "DIVISON"
     ) {
+        if (ops === "EQUALS") {
+            prevNum = parseFloat(numberInput.value);
+            numberInput.value = '';
+        }
         if (ops === "") {
-            console.log('terms: ', terms);
             ops = terms;
-            console.log('ops: ', ops);
-            prevNum = parseInt(numberInput.value);
-            console.log('prevNum: ', prevNum);
+            prevNum = parseFloat(numberInput.value);
             numberInput.value = ""
         } else {
             let result = operate();
@@ -36,26 +39,30 @@ function numpadHit(terms) {
     } else if (terms === "RESET") {
         ops = "";
         prevNum = 0;
-        numberInput.value = ""
+        numberInput.value = "";
     } else if (terms === "EQUALS") {
         let result = operate();
         if (result === "ERROR") {
             alert('Please specify value!')
         } else {
             numberInput.value = result;
-            ops = ""
+            prevNum = result;
+            ops = "EQUALS";
         }
-        isSolved = true;
     } else {
         numberInput.value = numberInput.value.concat(terms);
     }
+    console.log(`
+        CURRENT!
+        prevNum : ${prevNum},
+        ops : ${ops},
+        terms : ${terms},
+        `);
 }
 
 const operate = () => {
-    let op1 = parseInt(numberInput.value),
-        op2 = parseInt(prevNum);
-    console.log('op1: ', op1);
-    console.log('op2: ', op2);
+    let op1 = parseFloat(numberInput.value),
+        op2 = parseFloat(prevNum);
     if (ops === "ADDITION") return op2 + op1;
     if (ops === "SUBTRACTION") return op2 - op1;
     if (ops === "MULTIPLICATION") return op2 * op1;
